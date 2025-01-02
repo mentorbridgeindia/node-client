1fa676a92f0c31929cac3e1cafa49542b1693e75
NivethithaAbilash <nivethitha029@gmail.com>
2025-01-02 20:53:00 +0530

    reset removed

--- backend/Email/constants.ts
+++ backend/Email/constants.ts
@@ -2,6 +2,6 @@ export const EMAIL_TYPES = {
   REGISTER_EMAIL: "register_email",
   RESET_PASSWORD: "reset_password",
   WELCOME: "welcome",
-  RESET: "reset",
+  
   PASSWORD_CHANGED: "password_changed",
 };
