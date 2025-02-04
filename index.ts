27670d0f1a515325f6e8f11c4a3c368d85712089
NivethithaAbilash <nivethitha029@gmail.com>
2025-02-04 20:45:19 +0530

    index

--- index.ts
+++ index.ts
@@ -6,28 +6,36 @@ import { setupAddRoute } from "./backend/Routes/addRoute";
 import { setupDynamicRoutes } from "./backend/Routes/dynamicRoutes";
 import { cronJob } from "./backend/Scrap/cronJob";
 import { initiateScraping } from "./backend/Scrap/initiateScraping";
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 app.use(bodyParser.json());
 setupEmailRoutes(app);
-
+const swaggerDocument = require("./swagger.json");
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("client/public"));
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 app.get("/", (req, res) => {
   // res.sendFile(path.join(__dirname, "client/public/index.html"));
   res.send("Hello nodejs !");
 });
 
-setupDynamicRoutes(app);
-setupAddRoute(app);
+setupEmailRoutes(app);
+
+// setupDynamicRoutes(app);
+// setupAddRoute(app);
 // setupDefaultRoute(app);
 
 app.listen(4444, () => {
   console.log("Server is running on port 4444");
   // scrapeMedium("https://medium.com/tag/personal-development");
-  cronJob();
+  // cronJob();
   // initiateScraping();
 });
+
+
