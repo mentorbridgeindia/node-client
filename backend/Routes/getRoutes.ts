7fc54c1f9bacddffa258bd620eb10fc7fb680334
Mentor <mentorbridgeindia@gmail.com>
2025-04-19 13:07:49 +0530

    Modified node

--- backend/Routes/getRoutes.ts
+++ backend/Routes/getRoutes.ts
@@ -1,3 +1,4 @@
+import { MongoTopologyClosedError } from "mongodb";
 import { STUBLAB_DB, STUBLAB_DB_NAME, STUBLAB_DB_OPTIONS } from "../constants";
 import { switchDatabase } from "../Database/switchDatabase";
 import { MockApi } from "../Models/MockApi";
@@ -5,16 +6,29 @@ import { Organization } from "../Models/Organization";
 
 export const getRoutes = async (): Promise<any[]> => {
   const routes: any[] = [];
-  await switchDatabase(STUBLAB_DB + STUBLAB_DB_NAME + STUBLAB_DB_OPTIONS);
 
-  const organizations = await Organization.find().lean();
-  for (const organization of organizations) {
-    if (organization.dbName) {
-      await switchDatabase(
-        STUBLAB_DB + organization.dbName + STUBLAB_DB_OPTIONS
+  try {
+    // Connect to main database
+    await switchDatabase(STUBLAB_DB + STUBLAB_DB_NAME + STUBLAB_DB_OPTIONS);
+    const organizations = await Organization.find().lean();
+
+    // Process each organization's routes
+    for (const organization of organizations) {
+      if (organization.dbName) {
+        await switchDatabase(
+          STUBLAB_DB + organization.dbName + STUBLAB_DB_OPTIONS
+        );
+        const apiRoutes = await MockApi.find().lean();
+        routes.push(...apiRoutes);
+      }
+    }
+  } catch (error) {
+    if (error instanceof MongoTopologyClosedError) {
+      console.error(
+        "Attempted to check out a connection from closed connection pool"
       );
-      const apiRoutes = await MockApi.find().lean();
-      routes.push(...apiRoutes);
+    } else {
+      console.error(error);
     }
   }
 
